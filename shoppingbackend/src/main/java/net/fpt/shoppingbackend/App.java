8d4dcef8df89dc816847d286e48024eecdac5427
ViruSs <destinypham22@gmail.com>
2017-11-04 17:35:39 +0700

    My project setup successfull complie!

---
+++ shoppingbackend/src/main/java/net/fpt/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.fpt.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
